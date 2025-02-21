05938e459abe8e98733f43ac281a368e18ee5d9d
Камиль <@DESKTOP-VBU3J7I>
2025-02-21 10:38:51 +0300

    6 практика

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,156 +1,235 @@
 ﻿using System;
 using System.Linq;
-using System.Text;
 
 public class Methods
 {
-    public static void Main(string[] args)
+    // --- Этап 1: Calcul (2 числа) ---
+    public static (double plus, double minus, double product, string chastnoeMessage) Calcul(double a, double b)
     {
-        // 1. Сумма двух и трёх чисел
-        Console.WriteLine("1. Сумма двух и трёх чисел:");
-        Calcul(5, 3);
+        double plus = a + b;
+        double minus = a - b;
+        double product = a * b;
+        string chastnoeMessage;
+        double chastnoe;
 
-        // 2. Минимальное и максимальное значение
-        Console.WriteLine("\n2. Минимальное и максимальное значение:");
-        Console.WriteLine("Меньшее из 10 и 5: " + Min(10, 5));
-        Console.WriteLine("Большее из 10 и 5: " + Max(10, 5));
-
-        // 3. Проверка на палиндром
-        Console.WriteLine("\n3. Проверка на палиндром:");
-        Console.WriteLine("\"level\" - палиндром: " + IsPalindrome("level"));
-        Console.WriteLine("\"hello\" - палиндром: " + IsPalindrome("hello"));
-
-        // 4. Факториал числа
-        Console.WriteLine("\n4. Факториал числа:");
-        Console.WriteLine("Факториал 5: " + Factorial(5));
-
-        // 5. Среднее арифметическое
-        Console.WriteLine("\n5. Среднее арифметическое:");
-        Console.WriteLine("Среднее арифметическое 2, 4, 6: " + Average(2, 4, 6));
-
-        // 6. Поиск символа в строке
-        Console.WriteLine("\n6. Поиск символа в строке:");
-        Console.WriteLine("Индекс 'e' в \"hello\": " + FindChar("hello", 'e'));
-        Console.WriteLine("Индекс 'a' в \"hello\": " + FindChar("hello", 'a'));
-
-        // 7. Генерация случайного пароля (только цифры)
-        Console.WriteLine("\n7. Генерация случайного пароля (только цифры):");
-        Console.WriteLine("Случайный пароль длиной 6: " + GeneratePassword(6));
-
-        // 8. Конвертация температуры
-        Console.WriteLine("\n8. Конвертация температуры:");
-        Console.WriteLine("0 градусов Цельсия в Фаренгейты: " + CelsiusToFahrenheit(0));
-        Console.WriteLine("32 градуса Фаренгейта в Цельсии: " + FahrenheitToCelsius(32));
-
-        // 9. Перестановка слов в предложении
-        Console.WriteLine("\n9. Перестановка слов в предложении:");
-        Console.WriteLine("Перестановка слов в \"Hello world!\": " + ReverseWords("Hello world!"));
-
-        // 10. Таблица умножения для числа
-        Console.WriteLine("\n10. Таблица умножения для числа:");
-        MultiplicationTable(5);
-    }
-
-    // 1. Сумма двух и трёх чисел
-    static void Calcul(int a, int b)
-    {
-        Console.WriteLine("Сумма: " + (a + b));
-        Console.WriteLine("Разность: " + (a - b));
-        Console.WriteLine("Произведение: " + (a * b));
-        if (b != 0)
+        if (b == 0)
         {
-            Console.WriteLine("Частное: " + ((double)a / b));
+            chastnoe = 0;
+            chastnoeMessage = "Деление на 0 недопустимо!";
         }
         else
         {
-            Console.WriteLine("Деление на ноль!");
+            chastnoe = a / b;
+            chastnoeMessage = chastnoe.ToString();
+        }
+
+        return (plus, minus, product, chastnoeMessage);
+    }
+
+    // --- Этап 1.1: Calcul (3 числа) ---
+    public static (double plus3, double product3, string chastnoeMessage3) Calcul(double a, double b, double c)
+    {
+        double plus3 = a + b + c;
+        double product3 = a * b * c;
+        string chastnoeMessage3;
+        double chastnoe3;
+
+        if (b == 0 || c == 0)
+        {
+            chastnoe3 = 0;
+            chastnoeMessage3 = "Деление на 0 недопустимо!";
+        }
+        else
+        {
+            chastnoe3 = a / b / c;
+            chastnoeMessage3 = chastnoe3.ToString();
+        }
+
+        return (plus3, product3, chastnoeMessage3);
+    }
+
+    // --- Этап 2: Min ---
+    public static int Min(int chislo1, int chislo2)
+    {
+        if (chislo1 < chislo2)
+        {
+            return chislo1;
+        }
+        else
+        {
+            return chislo2;
         }
     }
 
-
-    // 2. Минимальное и максимальное значение
-    static int Min(int a, int b)
+    // --- Этап 3: Max ---
+    public static int Max(int chislo1, int chislo2)
     {
-        return a < b ? a : b;
+        if (chislo1 > chislo2)
+        {
+            return chislo1;
+        }
+        else
+        {
+            return chislo2;
+        }
     }
 
-    static int Max(int a, int b)
+    // --- Этап 4: IsPalindrome ---
+    public static bool IsPalindrome(string stroka)
     {
-        return a > b ? a : b;
+        string nizhniyRegistr = stroka.ToLower();
+        bool result = nizhniyRegistr.SequenceEqual(nizhniyRegistr.Reverse());
+        return result;
     }
 
-    // 3. Проверка на палиндром
-    static bool IsPalindrome(string str)
+    // --- Этап 5: Factorial ---
+    public static long Factorial(int chislo)
     {
-        str = str.ToLower(); // Преобразуем строку в нижний регистр, чтобы не учитывать регистр
-        string reversedStr = new string(str.ToCharArray().Reverse().ToArray());
-        return str == reversedStr;
-    }
-
-    // 4. Факториал числа
-    static long Factorial(int n)
-    {
-        if (n == 0)
+        if (chislo < 0)
+        {
+            return -1;
+        }
+        if (chislo == 0)
         {
             return 1;
         }
+
         long result = 1;
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i <= chislo; i++)
         {
             result *= i;
         }
         return result;
     }
 
-    // 5. Среднее арифметическое
-    static double Average(int a, int b, int c)
+    // --- Этап 6: Average ---
+    public static double Average(int chislo1, int chislo2, int chislo3)
     {
-        return (double)(a + b + c) / 3;
+        double result = (double)(chislo1 + chislo2 + chislo3) / 3;
+        return result;
     }
 
-    // 6. Поиск символа в строке
-    static int FindChar(string text, char c)
+    // --- Этап 7: FindChar ---
+    public static int FindChar(string text, char simvol)
     {
-        return text.IndexOf(c);
+        int result = text.IndexOf(simvol);
+        return result;
     }
 
-    // 7. Генерация случайного пароля (только цифры)
-    static string GeneratePassword(int length)
+    // --- Этап 8: GeneratePassword ---
+    public static string GeneratePassword(int dlina)
     {
         Random random = new Random();
-        StringBuilder password = new StringBuilder();
-        for (int i = 0; i < length; i++)
+        string parol = "";
+        for (int i = 0; i < dlina; i++)
         {
-            password.Append(random.Next(0, 10)); // Добавляем случайную цифру от 0 до 9
+            parol += random.Next(0, 10).ToString();
         }
-        return password.ToString();
+        string result = parol;
+        return result;
     }
 
-    // 8. Конвертация температуры
-    static double CelsiusToFahrenheit(double c)
+    // --- Этап 9: CelsiusToFahrenheit ---
+    public static double CelsiusToFahrenheit(double c)
     {
-        return c * 9 / 5 + 32;
+        double result = c * 9 / 5 + 32;
+        return result;
     }
 
-    static double FahrenheitToCelsius(double f)
+    // --- Этап 10: FahrenheitToCelsius ---
+    public static double FahrenheitToCelsius(double f)
     {
-        return (f - 32) * 5 / 9;
+        double result = (f - 32) * 5 / 9;
+        return result;
     }
 
-    // 9. Перестановка слов в предложении
-    static string ReverseWords(string sentence)
+    // --- Этап 11: ReverseWords ---
+    public static string ReverseWords(string predlozhenie)
     {
-        string[] words = sentence.Split(' ');
+        string[] words = predlozhenie.Split(' ');
         Array.Reverse(words);
-        return string.Join(" ", words);
+        string result = string.Join(" ", words);
+        return result;
     }
 
-    // 10. Таблица умножения для числа
-    static void MultiplicationTable(int n)
+    // --- Этап 12: MultiplicationTable ---
+    public static void MultiplicationTable(int chislo)
     {
         for (int i = 1; i <= 10; i++)
         {
-            Console.WriteLine(n + " x " + i + " = " + (n * i));
+            Console.WriteLine(chislo + " x " + i + " = " + (chislo * i));
         }
+    }
+
+    // --- Этап 13: Main ---
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        if (!double.TryParse(Console.ReadLine(), out double a))
+        {
+            Console.WriteLine("Некорректный ввод первого числа.");
+            return;
+        }
+
+        Console.Write("Введите второе число: ");
+        if (!double.TryParse(Console.ReadLine(), out double b))
+        {
+            Console.WriteLine("Некорректный ввод второго числа.");
+            return;
+        }
+
+        (double plus, double minus, double product, string chastnoeMessage) result1 = Calcul(a, b);
+        Console.WriteLine("Сумма: " + result1.plus + ", Разность: " + result1.minus + ", Произведение: " + result1.product + ", Частное: " + result1.chastnoeMessage);
+        Console.WriteLine("________________");
+
+        Console.Write("Введите третье число: ");
+        if (!double.TryParse(Console.ReadLine(), out double c))
+        {
+            Console.WriteLine("Некорректный ввод третьего числа.");
+            return;
+        }
+
+        (double plus3, double product3, string chastnoeMessage3) resultCalcul3 = Calcul(a, b, c);
+        Console.WriteLine("Сумма 3 чисел: " + resultCalcul3.plus3 + ", Произведение 3 чисел: " + resultCalcul3.product3 + ", Частное 3 чисел: " + resultCalcul3.chastnoeMessage3);
+        Console.WriteLine("________________");
+
+        Console.Write("Введите первое число для сравнения: ");
+        if (!int.TryParse(Console.ReadLine(), out int chislo1))
+        {
+            Console.WriteLine("Некорректный ввод первого числа для сравнения.");
+            return;
+        }
+
+        Console.Write("Введите второе число для сравнения: ");
+        if (!int.TryParse(Console.ReadLine(), out int chislo2))
+        {
+            Console.WriteLine("Некорректный ввод второго числа для сравнения.");
+            return;
+        }
+
+        int minimum = Min(chislo1, chislo2);
+        Console.WriteLine("Минимум из " + chislo1 + " и " + chislo2 + ": " + minimum);
+
+        int maximum = Max(chislo1, chislo2);
+        Console.WriteLine("Максимум из " + chislo1 + " и " + chislo2 + ": " + maximum);
+
+        Console.WriteLine("________________");
+
+        Console.WriteLine("Является ли 'level' палиндромом: " + IsPalindrome("level"));
+        Console.WriteLine("Факториал числа 5: " + Factorial(5));
+
+        Console.WriteLine("Среднее арифметическое 2, 4, 6: " + Average(2, 4, 6));
+
+        Console.WriteLine("Индекс 'e' в 'hello': " + FindChar("hello", 'e'));
+
+        Console.WriteLine("Случайный пароль длиной 8: " + GeneratePassword(8));
+
+        Console.WriteLine("0 градусов Цельсия в Фаренгейтах: " + CelsiusToFahrenheit(0));
+        Console.WriteLine("32 градуса Фаренгейта в Цельсиях: " + FahrenheitToCelsius(32));
+
+        Console.WriteLine("Перестановка слов в 'Hello world!': " + ReverseWords("Hello world!"));
+
+        Console.WriteLine("Таблица умножения для числа 5:");
+        MultiplicationTable(5);
     }
 }
