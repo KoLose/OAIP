63ddd0a6ef1c5d87c0ee0230474b7dccbeba41ec
KoLose8 <KoLose8@DESKTOP-I4G6VC0>
2025-03-23 20:19:53 +0300

    Deleted edge methods

--- morskoiBoy/Program.cs
+++ morskoiBoy/Program.cs
@@ -11,53 +11,28 @@ namespace morskoiBoy
 {
     internal class Program
     {
-
-        //ЗАПОЛНЕНИЕ ЦВЕТНОГО МАССИВА ИГРОКА
-        static string[,] FillColorPlayerArray(string[,] colorPlayerArray)
+        //ЗАПОЛНЕНИЕ МАССИВОВ
+        static string[,] FillArray(string[,] array)
         {
-            for (int i = 0; i < colorPlayerArray.GetLength(0); i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    colorPlayerArray[i, j] = "*";
+                    array[i, j] = "*";
                 }
             }
-            return colorPlayerArray;
+            return array;
         }
 
-        //ЗАПОЛНЕНИЕ МАССИВА БОТА
-        static string[,] FillBotArray(string[,] botArray)
+        //ЗАПОЛНЕНИЕ СЛОВАРЕЙ
+        static Dictionary<string, int> FillShips(Dictionary<string, int> dictionaryShips)
         {
-            for (int i = 0; i < botArray.GetLength(0); i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    botArray[i, j] = "*";
-                }
-            }
-            return botArray;
-        }
+            dictionaryShips.Add("4-х палубный", 1);
+            dictionaryShips.Add("3-х палубный", 2);
+            dictionaryShips.Add("2-х палубный", 3);
+            dictionaryShips.Add("1 палубный", 4);
 
-        //ЗАПОЛНЕНИЕ СЛОВАРЯ КОРАБЛЕЙ ИГРОКА
-        static Dictionary<string, int> FillPlayerShips(Dictionary<string, int> playerShips)
-        {
-            playerShips.Add("4-х палубный", 1);
-            playerShips.Add("3-х палубный", 2);
-            playerShips.Add("2-х палубный", 3);
-            playerShips.Add("1 палубный", 4);
-
-            return playerShips;
-        }
-
-        //ЗАПОЛНЕНИЕ СЛОВАРЯ КОРАБЛЕЙ БОТА
-        static Dictionary<string, int> FillBotShips(Dictionary<string, int> botShips)
-        {
-            botShips.Add("4-х палубный", 1);
-            botShips.Add("3-х палубный", 2);
-            botShips.Add("2-х палубный", 3);
-            botShips.Add("1 палубный", 4);
-
-            return botShips;
+            return dictionaryShips;
         }
 
         //РАССТАНОВКА КОРАБЛЕЙ ИГРОКА
@@ -549,10 +524,9 @@ namespace morskoiBoy
                     Console.BackgroundColor = ConsoleColor.DarkBlue;
                 }
                 Console.ResetColor();
-                Console.Write("  " + (i + 1));
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.BackgroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine();
+                Console.WriteLine("  " + (i + 1));
+                //Console.ForegroundColor = ConsoleColor.White;
+                //Console.BackgroundColor = ConsoleColor.DarkBlue;
             }
             Console.ResetColor();
             Console.WriteLine("----------------------------");
@@ -561,7 +535,7 @@ namespace morskoiBoy
         static void Main(string[] args)
         {
             //СОЗДАЁМ МАССИВЫ
-            string[,] colorPlayerArray = new string[10, 11];
+            string[,] playerArray = new string[10, 11];
             string[,] botArray = new string[10, 11];
 
             //СОЗДАЁМ СЛОВАРИ
@@ -569,17 +543,17 @@ namespace morskoiBoy
             Dictionary<string, int> botShips = new Dictionary<string, int>();
 
             //ЗАПОЛНЯЕМ МАССИВЫ
-            FillColorPlayerArray(colorPlayerArray);
-            FillBotArray(botArray);
+            FillArray(playerArray);
+            FillArray(botArray);
 
             //ЗАПОЛНЯЕМ СЛОВАРИ
-            FillPlayerShips(playerShips);
-            FillBotShips(botShips);
+            FillShips(playerShips);
+            FillShips(botShips);
 
             //ИНИЦИАЛИЗАЦИЯ ИГРЫ
             Console.WriteLine("Добро пожаловать в игру morskoiBoy");
-            ArrangePlayerArray(colorPlayerArray, playerShips);
-            CheckPlayerArray(colorPlayerArray, 0, 0, playerShips);
+            ArrangePlayerArray(playerArray, playerShips);
+            //CheckPlayerArray(playerArray, 0, 0, playerShips);
             Console.ReadKey();
 
         }
